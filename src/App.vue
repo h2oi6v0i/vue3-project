c2b9810d7db536729abcf8b9fb79b3ea99ce42fd
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-06-20 23:03:43 +0900

    [0-8] : 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div :class="nameClass">{{ name }}</div>
+    <input :type="type" :value="name" />
+    <button class="btn btn-primary" @click="updateName">Click</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    setup() {
+        const name = ref("Gary");
+        const type = ref("text");
+        const nameClass = ref("");
+
+        const updateName = () => {
+            name.value = "Jeon Gary";
+            nameClass.value = "name";
+        };
+
+        return {
+            name,
+            type,
+            nameClass,
+            updateName,
+        };
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.name {
+    color: cornflowerblue;
+    font-weight: 700;
 }
 </style>
